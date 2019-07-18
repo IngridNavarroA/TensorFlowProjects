6cc3401e2dcea8d241c4f7abbdf3a497617ac45c
IngridNavarroA <ingrid.navarro.an@gmail.com>
2019-07-17 21:43:17 -0500

    Started Image Classifier API

--- README.md
+++ README.md
@@ -1,1 +1,152 @@
-# TensorFlow_Projects
+# TensorFlow Image Classifier API 
+
+This is a simple image classifier API to train and test image classifier models. 
+
+Supported models:
+- AlexNet 
+- VGG16
+- Inception (In progress)
+- ResNet (In progress)
+- SqueezeNet (In progress)
+
+You can also implement your own model using the layer wrappers, and add more wrappers as you need them. 
+
+The API allows to perform end-to-end training, finetuning and restoring a training process. I am implementing a GUI to test images on trained models. In the meantime, use the test.py if you want to test a given model.
+
+## Requirements and installation
+
+This code has been implemented / tested / maintained with:
+- OpenCV == 3.4
+- Python3 
+- Tensorflow == 1.13.1
+
+Installing a virtual environment using python3 using bash:
+```
+	sudo pip3 install virtualenv virtualenvwrapper
+	echo "# Virtual Environment Wrapper"  >> ~/.bashrc
+	echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+	source ~/.bashrc
+```
+Or using zsh:
+```
+	sudo pip3 install virtualenv virtualenvwrapper
+	echo "# Virtual Environment Wrapper"  >> ~/.zshrc
+	echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.zshrc
+	source ~/.zshrc
+```
+
+Create virtual environment with python3:
+```
+	mkvirtualenv tf-classifier -p python3
+	workon tf-classifier
+```
+
+Test python version on virtual enviornment:
+```
+	workon tf-classifier
+	python -V
+	deactivate
+```
+
+Also, I provided a list of requirements, which you can install as follows: 
+```
+	workon tf-classifier
+	pip install -r requirements.txt
+```
+
+Note: OpenCV is not included in these requirements. I follow this [tutorial](https://www.learnopencv.com/install-opencv3-on-ubuntu/) to install it, except I used version 3.4.
+
+## Utils
+
+I designed a couple of utilities to fetch and prepare the dataset for training. 
+
+### Data Fetch 
+
+This is to fetch a dataset from the server where I stored the images. 
+
+```
+	python utils/fetch_dataset_parallel.py --outpath data/
+
+```
+
+Or
+
+```
+	python utils/fetch_dataset_serial.py --outpath data/
+```
+
+# ---------------------------------------------
+# ADD TO README: Classification framework and Training process. 
+
+### Data Processing 
+
+These are scripts to change dataset colorspaces and perform offline data augmentation. 
+
+```
+	python utils/preprocess.py --inpath path/to/input/data --outpath path/to/output/data --crop v
+```
+
+### Data Split
+
+This code is to split data into train and test sets. Furthermore, the train set is divided into train and validation during the training process. 
+
+```
+	python utils/split.py --inpath path/to/input/data --opath path/to/output/data --test_set 0.2
+```
+
+
+### Entrenamiento de la red (end-to-end)
+```
+	workon tf-classifier
+  python src/classifier/train.py --data path/to/training/data --max_iter 20000 --gpu 0 --restore 0 --fintune 0
+```
+
+### Finetunning de la red
+```
+	workon tf-classifier
+	python src/train.py --data path/to/training/data --max_iter 20000 --gpu 0 --restore 0 --fintune 1
+```
+
+### Restaurar entrenamiento de la red
+```
+	workon tf-classifier
+	python src/train.py --data path/to/training/data --max_iter 20000 --gpu 0 --restore 1
+```
+
+### Visualización de entrenamiento 
+```
+	workon tf-classifier
+	tensorboard --logdir=log
+```
+
+### Prueba de modelo entrenado
+```
+	workon tf-classifier
+	python src/test.py --data path/to/testing/data --meta path/to/model
+```
+
+Al abrir en la dirección de loopback (e.g. http://127.0.1.1:6006) indicada por TensorBoard, se observarán gráficas como las siguientes:
+
+### Accuracy
+<p align="center"><img src="./readme/accuracy.png" /> </p>
+
+### Loss
+<p align="center"><img src="./readme/loss.png" /> </p>
+
+## Arquitectura actual del clasificador
+<p align="center"><img src="./readme/alexnet.png" /> </p>
+
+
+### TODOS:
+-[x] Implementar Alexnet 
+-[x] Implementar VGG16 con entrenamiento end-to-end, restore y finetuning 
+-[ ] Implementar ResNet con entrenamiento end-to-end, restore y finetuning 
+-[ ] Implementar Inception con entrenamiento end-to-end, restore y finetuning 
+-[ ] Implementar SqueezeNet con entrenamiento end-to-end, restore y finetuning 
+-[ ] Conseguir modelo pre-entreando para Inception, ResNet y SqueezeNet
+-[ ] Implementar finetuning para Inception
+-[ ] Documentacion de codigo - wiki
+-[ ] Agregar bash scripts para correr pruebas las redes
+-[ ] Agregar funcionalidad de entrenamiento multiescala 
+-[ ] Implement GUI to test models
+-[ ] Modular data fetch
---
+++ classifier/layers.py
@@ -0,0 +1,127 @@
+"""
+	@author: Ingrid Navarro 
+	@date:   May 10th, 2019
+	@brief:  Layer wrappers 
+"""
+import tensorflow as tf
+
+def variable_summary(var):
+	""" Wrapper that adds summary to Tensor variable to be visualized 
+		with TensorBoard. """
+	
+	with tf.name_scope('summaries'):
+		mean = tf.reduce_mean(var)
+		tf.summary.scalar('mean', mean)
+		
+		with tf.name_scope('stddev'):
+			stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+		
+		# Visualizing gradient  
+		tf.summary.scalar('stddev', stddev)
+		tf.summary.scalar('max', tf.reduce_max(var))
+		tf.summary.scalar('min', tf.reduce_min(var))
+		
+		# Visualizing activation distribution
+		tf.summary.histogram('histogram', var)
+
+def conv(name, x, fsize, nfilters, stride=1, padding='SAME', groups=1, stddev=0.05, binit_val=0.05):
+	""" Wrapper for Convolutional layer. """
+	
+	ninputs = int(x.get_shape()[-1].value / groups)
+	convolve = lambda i, w: tf.nn.conv2d(i, w, 
+		strides=[1, stride, stride, 1], padding=padding)
+
+	with tf.variable_scope(name) as scope:
+
+		# Create random weights 
+		w_init = tf.truncated_normal(shape=[fsize, fsize, ninputs, nfilters], stddev=stddev, dtype=tf.float32)
+		w = tf.get_variable('weights', initializer=w_init, dtype=tf.float32)
+
+		# Initialize biases 
+		b_init = tf.constant(binit_val, shape=[nfilters], dtype=tf.float32)
+		b = tf.get_variable('biases', initializer=b_init, dtype=tf.float32)
+
+		# Convolution 
+		if groups == 1:
+			layer = convolve(x, w)
+		else:
+			x_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
+			w_groups = tf.split(axis=3, num_or_size_splits=groups, value=w)
+
+			out_groups = [convolve(i, k) for i, k in zip(x_groups, w_groups)]
+			layer = tf.concat(axis=3, values=out_groups)
+
+		layer += b
+
+		print("\t[LAYER] {} has shape {}".format(name, layer.shape))
+		return tf.nn.relu(layer, name=scope.name)
+
+def lrn(x, radius, alpha, beta, bias=1.0):
+	""" Wrapper for Local Response Normalization layer (LRN). """
+	return tf.nn.local_response_normalization(x,
+		depth_radius=radius, alpha=alpha, beta=beta, bias=bias)
+
+def maxpool(name, x, fsize=3, stride=2, padding='SAME'):
+	""" Wrapper for Pooling layer. """
+	with tf.variable_scope(name):
+		layer = tf.nn.max_pool(value=x, ksize=[1, fsize, fsize, 1], 
+			strides=[1, stride, stride, 1], padding=padding)       
+		
+		variable_summary(layer)
+
+		print("\t[LAYER] {} has shape {}".format(name, layer.shape))	
+		return layer
+
+def flatten(x):
+	""" Wrapper to convert mutlidimensional tensor into a 
+		one-dimension tensor. """
+	layer_shape = x.get_shape()
+	nfeatures = layer_shape[1:].num_elements()
+	print("\t[LAYER] flatted to {} features".format(nfeatures))
+	return tf.reshape(x, [-1, nfeatures])
+
+def fc(name, x, noutputs, relu=True, stddev=0.05, binit_val=0.05):
+	""" Wrapper for Fully Connected layer. """ 
+	
+	layer_shape = x.get_shape()
+	ninputs = layer_shape[1:].num_elements()
+
+	with tf.variable_scope(name) as scope:
+		# Create random weights
+		w_init = tf.truncated_normal(shape=[ninputs, noutputs], stddev=stddev, dtype=tf.float32)
+		w = tf.get_variable('weights', initializer=w_init, dtype=tf.float32)
+
+		# Initialize bias 
+		b_init = tf.constant(binit_val, shape=[noutputs], dtype=tf.float32)
+		b = tf.get_variable('biases', initializer=b_init, dtype=tf.float32)
+
+		layer = tf.nn.xw_plus_b(x, w, b, name=scope.name)
+
+		if relu:
+			layer = tf.nn.relu(layer)
+		print("\t[LAYER] {} has shape {}".format(name, layer.shape))
+		return layer
+
+def dropout(x, keep_prob=0.5):
+	""" Wrapper for Dropout layer. """
+	return tf.nn.dropout(x, rate=1-keep_prob)
+
+
+def inception(name, x, conv1_size, conv3_red_size, conv3_size, conv5_red_size, conv5_size, pool_proj_size):
+	""" Inception module. """
+	with tf.variable_scope(name) as scope:
+		
+		print("\n\t[MODULE] {}".format(name))
+		conv1 = conv('{}_1x1'.format(name), x=x, fsize=1, nfilters=conv1_size)
+		
+		conv3_red = conv('{}_3x3_red'.format(name), x=x, fsize=1, nfilters=conv3_red_size)
+		conv3 = conv('{}_3x3'.format(name), x=conv3_red, fsize=3, nfilters=conv3_size)
+		
+		conv5_red = conv('{}_5x5_red'.format(name), x=x, fsize=1, nfilters=conv5_red_size)
+		conv5 = conv('{}_5x5'.format(name), x=conv5_red, fsize=5, nfilters=conv5_size)
+		
+		pool = maxpool('{}_pool'.format(name), x=x, fsize=1, stride=1, padding='VALID')
+		pool_proj = conv('{}_pool_proj'.format(name), x=pool, fsize=1, nfilters=pool_proj_size)
+
+		return tf.concat([conv1, conv3, conv5, pool_proj], axis=3, name='{}_concat'.format(name))
+
