b4facee172932500828601c83fba17fc57b193e2
navarrs <ingrid.navarro.an@gmail.com>
2019-09-27 22:59:35 -0500

    fixed deprecations :/

--- classifier/train.py
+++ classifier/train.py
@@ -40,9 +40,11 @@ tf.app.flags.DEFINE_integer('meta', 0, """Save metafiles [1 / 0]. """)
 tf.app.flags.DEFINE_integer('gpu', 0, """What GPU (ID) to use. """)
 tf.app.flags.DEFINE_integer('cpu', 0, """Use CPU [1 / 0]. """)
 
-def get_current_iteration(epoch:int, iter_batch:int) -> int:
+tf.app.flags.DEFINE_boolean("verbose", False, """Set to debug program.""")
+
+def get_current_iteration( epoch, iter_batch ):
 	""" If restoring a model, return the last iteration. """
-	return int(epoch * iter_batch)
+	return int( epoch * iter_batch )
 
 
 def train():
@@ -82,12 +84,8 @@ def train():
 
 	sess = tf.compat.v1.Session()
 
-	# Inputs 
 	x = tf.compat.v1.placeholder(tf.float32, 
 		      shape=[None, cfg.img_height, cfg.img_width, cfg.img_depth], name='x')
-	y = tf.compat.v1.placeholder(tf.float32, 
-		      shape=[None, cfg.num_classes], name='y')
-	y_cls = tf.argmax(y, axis=1)
 	
 	info_msg("Loading architecture {}...".format(FLAGS.net))
 	if FLAGS.net.lower() == 'alexnet':
@@ -97,121 +95,115 @@ def train():
 	elif FLAGS.net == 'vgg':
 		model = N.VGG16(cfg.NUM_CLS)
 		out   = model.load_net(x, training=True)
+	
 	model_dict = cfg.net_dict
-
-	# elif FLAGS.net == 'inception':
-	# 	model = N.Inception(cfg.NUM_CLS)
-	# 	out = model.load_net(x, training=True)
-	# 	model_dict = cfg.INCEPTION
-
 	done_msg()
 
-	# y_pred = tf.nn.softmax(out, name='y_pred')
-	# y_pred_cls = tf.argmax(y_pred, axis=1)
+	# Output 
+	y = tf.compat.v1.placeholder( tf.float32, shape=[None, cfg.num_classes], name='y')
+	y_cls = tf.argmax( y, axis=1 )
+	y_hat = tf.nn.softmax( out, name='y_hat' )
+	y_hat_cls = tf.argmax( y_hat, axis=1 )
 
-	# # Variables to train 
-	# var_list = [v for v in tf.trainable_variables()]
+	# Get all trainable variables 
+	var_list = [ v for v in tf.compat.v1.trainable_variables() ]
 
-	# # Cost
-	# with tf.name_scope('cross_entropy'):
-	# 	cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=out, labels=y)
-	# 	cost = tf.reduce_mean(cross_entropy)
-	# 	tf.summary.scalar('cross_entropy', cost)
+	with tf.name_scope('loss'):
+		cost = tf.nn.softmax_cross_entropy_with_logits_v2( logits=out, labels=y )
+		cost = tf.reduce_mean( cost )
+		tf.compat.v1.summary.scalar( 'softmax_cross_entropy', cost )
 
-	# # Optimizer 
-	# with tf.name_scope('optimizer'):
-	# 	gradients = tf.gradients(cost, var_list)
-	# 	gradients = list(zip(gradients, var_list))
-	# 	optimizer = tf.train.GradientDescentOptimizer(cfg.LEARNING_RATE)
-	# 	train_opt = optimizer.apply_gradients(grads_and_vars=gradients)
-
-	# # Accuracy
-	# with tf.name_scope('accuracy'):
-	# 	correct_pred = tf.equal(y_pred_cls, y_cls)
-	# 	accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-	# 	tf.summary.scalar('accuracy', accuracy)
-
-	# train_writer = tf.summary.FileWriter(FLAGS.log + 'train', sess.graph)
-	# val_writer = tf.summary.FileWriter(FLAGS.log + 'val', sess.graph)
-	# merge = tf.summary.merge_all()
-	# saver = tf.train.Saver(max_to_keep=FLAGS.ckpts_to_keep)
-	# curr_iter, iter_batch = 0, int(train_set.num_examples / cfg.BATCH_SIZE)
-
-	# if FLAGS.finetune and not FLAGS.restore:
-	# 	""" Finetuning """
-	# 	info_msg("Finetuning {} network layers {} ..."
-	# 		.format(FLAGS.net, model_dict["train_layers"]))
-	# 	model.load_weights(model_dict["weights"], model_dict["train_layers"], sess)
-	# 	sess.run(tf.global_variables_initializer())
-	# 	done_msg()
-	# elif FLAGS.restore:
-	# 	""" Training from model checkpoint. """
-	# 	info_msg("Loading checkpoint for {} network from {} ..."
-	# 		.format(FLAGS.net, FLAGS.model))
-	# 	try:
-	
-	# 		saver = tf.train.import_meta_graph(model_dict["meta_file"])
-	# 		checkpoint = tf.train.get_checkpoint_state(FLAGS.model)
-	# 		checkpoint = checkpoint.model_checkpoint_path
-	# 		saver.restore(sess, checkpoint)
-	# 		sess.run(tf.global_variables())
-
-	# 		# Get current iteration from restored model 
-	# 		curr_iter = get_current_iteration(int(checkpoint.split('-')[-1]), iter_batch)
-	# 		curr_iter = 0 if curr_iter > int(cfg.RESTORE * FLAGS.max_iter) else curr_iter
-
-	# 	except:
-	# 		err_msg("Could not load checkpoint.")
-	# else: 
-	# 	""" Training form scratch """
-	# 	sess.run(tf.global_variables_initializer())
-	# 	info_msg("Performing end-to-end training.")
-
-	# # Training 
-	# info_msg("Starting training...")
-	# start = t.time()
-	# for i in range(curr_iter, FLAGS.max_iter):
-
-	# 	# Get next batches
-	# 	train_x, train_y = train_set.next_batch(cfg.BATCH_SIZE)
-	# 	train_dict = {x: train_x, y: train_y}
+	with tf.name_scope('optimizer'):
+		optimizer = tf.compat.v1.train.AdamOptimizer( 
+			cfg.learning_rate ).minimize( cost, var_list = var_list )
 		
-	# 	val_x, val_y = val_set.next_batch(cfg.BATCH_SIZE)
-	# 	val_dict = {x: val_x, y: val_y}
+	with tf.name_scope('accuracy'):
+		correct_pred = tf.cast( tf.equal( y_hat_cls, y_cls ), tf.float32 )
+		accuracy = tf.reduce_mean( correct_pred )
+		tf.compat.v1.summary.scalar( 'accuracy', accuracy )
 
-	# 	sess.run(train_opt, feed_dict=train_dict)
+	train_writer = tf.compat.v1.summary.FileWriter( FLAGS.log + 'train', sess.graph )
+	val_writer = tf.compat.v1.summary.FileWriter( FLAGS.log + 'val', sess.graph )
+	merge = tf.compat.v1.summary.merge_all()
+	saver = tf.compat.v1.train.Saver( max_to_keep=FLAGS.ckpts_to_keep )
+	
+	curr_iter, iter_batch = 0, int( train_set.num_examples / cfg.batch_size )
 
-	# 	if i % iter_batch == 0:
-	# 		# Compute loss 
-	# 		val_loss = sess.run(cost, feed_dict=val_dict)
-	# 		train_loss = sess.run(cost, feed_dict=train_dict)
+	if FLAGS.finetune and not FLAGS.restore:
+		""" Finetuning """
+		info_msg( "Finetuning {} layers {} ..."
+			.format( FLAGS.net, model_dict["train_layers"] ) )
+		model.load_weights( model_dict["weights"], model_dict["train_layers"], sess)
+		sess.run( tf.compat.v1.global_variables_initializer() )
+		done_msg()
 
-	# 		epoch = int(i / iter_batch)
+	elif FLAGS.restore:
+		""" Training from model checkpoint. """
+		info_msg( "Loading checkpoint for {} network from {} ..."
+			.format( FLAGS.net, FLAGS.model ) )
+		try:
+			saver = tf.train.import_meta_graph( model_dict["meta_file"] )
+			checkpoint = tf.train.get_checkpoint_state( FLAGS.model )
+			checkpoint = checkpoint.model_checkpoint_path
+			saver.restore( sess, checkpoint )
+			sess.run( tf.global_variables() )
 
-	# 		# Show progress 
-	# 		summary, train_acc = sess.run([merge, accuracy], feed_dict=train_dict)
-	# 		train_writer.add_summary(summary, epoch)
+			# Get current iteration from restored model 
+			curr_iter = get_current_iteration( int( checkpoint.split('-')[-1]), iter_batch )
+			curr_iter = 0 if curr_iter > int(cfg.restore_lim * FLAGS.max_iter ) else curr_iter
+		except:
+			err_msg("Could not load checkpoint.")
+	else: 
+		""" Training form scratch """
+		sess.run( tf.compat.v1.global_variables_initializer())
+		info_msg("Performing end-to-end training.")
 
-	# 		summary, val_acc = sess.run([merge, accuracy], feed_dict=val_dict)
-	# 		val_writer.add_summary(summary, epoch)
+	# Training 
+	info_msg("Starting training...")
+	start = t.time()
+	for i in range( curr_iter, FLAGS.max_iter ):
 
-	# 		msg = "\tEPOCH {0} --Train[Acc:{1:>6.1%}, Loss:{2:.3f}] --Val[Acc:{3:>6.1%}, Loss:{4:.3f}] --Time:{5:.3f} --[{6} / {7}]"
-	# 		done_msg(msg.format(epoch+1, train_acc, train_loss, val_acc, val_loss, t.time()-start, i, FLAGS.max_iter))
-	# 		start = t.time()
+		# Get next batches
+		train_x, train_y = train_set.next_batch( cfg.batch_size )
+		train_dict = { x: train_x, y: train_y }
+		
+		val_x, val_y = val_set.next_batch( cfg.batch_size )
+		val_dict = { x: val_x, y: val_y }
 
-	# 		saver.save(sess, FLAGS.model+'model-epoch-{}'.format(epoch), write_meta_graph=FLAGS.meta)
+		sess.run( optimizer, feed_dict=train_dict )
 
-	# done_msg("Finished training.")
+		if i % iter_batch == 0:
+			epoch = int( i / iter_batch )
+
+			# Show progress 
+			summary, train_loss = sess.run( [ merge, cost ], feed_dict=train_dict )
+			train_writer.add_summary( summary, epoch )
+			summary, train_acc = sess.run( [ merge, accuracy ], feed_dict=train_dict )
+			train_writer.add_summary( summary, epoch )
+
+			summary, val_loss   = sess.run( [ merge, cost ], feed_dict=val_dict )
+			val_writer.add_summary( summary, epoch )
+			summary, val_acc = sess.run( [ merge, accuracy ], feed_dict=val_dict )
+			val_writer.add_summary( summary, epoch )
+
+			msg = "\tEPOCH {0} --Train[Acc:{1:>6.1%}, Loss:{2:.3f}] --Val[Acc:{3:>6.1%}, Loss:{4:.3f}] --Time:{5:.3f} --[{6} / {7}]"
+			done_msg(msg.format(epoch+1, train_acc, train_loss, val_acc, val_loss, t.time()-start, i, FLAGS.max_iter))
+			start = t.time()
+
+			saver.save(sess, FLAGS.model+'model-epoch-{}'.format(epoch), write_meta_graph=FLAGS.meta)
+
+	done_msg("Finished training.")
 
 def main(argv=None):
 	if FLAGS.restore == 0:
+
 		if tf.io.gfile.exists(FLAGS.log):
-			tf.io.gfile.DeleteRecursively(FLAGS.log)
-			tf.io.gfile.MakeDirs(FLAGS.log)
+		 	tf.io.gfile.rmtree(FLAGS.log)
+		tf.io.gfile.makedirs(FLAGS.log)
 
 		if tf.io.gfile.exists(FLAGS.model):
-			tf.io.gfile.DeleteRecursively(FLAGS.model)
-			tf.io.gfile.MakeDirs(FLAGS.model)
+			tf.io.gfile.rmtree(FLAGS.model)
+		tf.io.gfile.makedirs(FLAGS.model)
 	train()
 
 if __name__ == '__main__':
