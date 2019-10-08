270842db4acceda07d4d7aed0a1c78c6bf8072b2
navarrs <ingrid.navarro.an@gmail.com>
2019-10-07 23:44:44 -0500

    documented code

--- tools/rename.py
+++ tools/rename.py
@@ -1,7 +1,8 @@
 """
 	@author: navarrs
 	@date:   October 2nd, 2019
-	@brief: Script renames files to base name.
+	@brief:  This script renames files to base name and adds the date and hour as 
+					 part of the name to avoid overwriting images. 
 """
 from datetime import datetime
 from skimage import io
@@ -10,28 +11,33 @@ import glob
 import cv2
 import os 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("--inpath", default="data", help="Path to data")
-ap.add_argument("--name",   default="img",  help="Base name for image files")
-ap.add_argument("--format",   default="jpg",  help="Image format")
-args = ap.parse_args()
+def rename( files, args ):
+	# This method takes a list of image filenames and renames them using a base
+	# name and the current date and hour.
+	processed_files, correct_files = 1, 0
+	for file in files:
+		filename = "{}_{}_{}.{}".format( args.name, 
+			                               correct_files, 
+			                               datetime.now().strftime("%d-%m-%Y_%H:%M:%S"), 
+			                               args.format )
+		try:
+			img  = io.imread( file )
+			cv2.imwrite( os.path.join( args.inpath, filename ), img )
+			correct_files += 1
+		except:
+			print( "Image {} could not be read".format( file ) )
 
-files = [f for f in glob.glob(args.inpath+"/*.*", recursive=True)]
+		os.remove( file )
+		print( "Total files: {}, Correct files: {} Processed files: {}".format(
+			     len( files ), correct_files, processed_files ) )
+		processed_files += 1
 
-processed_files, correct_files = 1, 1
-for file in files:
-	filename = "{}_{}_{}.{}".format(args.name, 
-		                              correct_files, 
-		                              datetime.now().strftime("%d-%m-%Y_%H:%M:%S"), 
-		                              args.format)
-	try:
-		img  = io.imread(file)
-		cv2.imwrite(os.path.join(args.inpath, filename), img)
-		correct_files += 1
-	except:
-		print("Image {} could not be read".format(file))
+if __name__ == '__main__':
+	ap = argparse.ArgumentParser()
+	ap.add_argument( "--inpath", default="data", help="Path to data" )
+	ap.add_argument( "--name",   default="img",  help="Base name for image files" )
+	ap.add_argument( "--format",   default="jpg",  help="Image format" )
+	args = ap.parse_args()
 
-	os.remove(file)
-	print("Total files: {}, Correct files: {} Processed files: {}".format(
-		     len(files), correct_files, processed_files))
-	processed_files += 1
+	files = [f for f in glob.glob( args.inpath+"/*.*", recursive=True )]
+	rename( files, args )
